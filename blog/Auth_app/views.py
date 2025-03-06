075e5c408a18f4e2e77dbd06bfdf9e4962437812
Jay <jaydwivedi.programmer@gmail.com>
2025-03-06 16:00:45 -0500

    Session 3

--- blog/Auth_app/views.py
+++ blog/Auth_app/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def print_hello(request):
+    return HttpResponse("Hello world")
+
+def home_page(request):
+    return render(request,"Auth_app/index.html")
--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMP_DIR= os.path.join(BASE_DIR,'Templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -57,7 +59,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMP_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS=(
+    os.path.join(BASE_DIR,'Static'),
+    )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
