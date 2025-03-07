ee56c5cceb41a9cfbb03e729df284adf28468dbb
Jay <jaydwivedi.programmer@gmail.com>
2025-03-07 15:25:09 -0500

    Session 3

--- blog/Auth_app/admin.py
+++ blog/Auth_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from . models import Contact
 # Register your models here.
+
+admin.site.register(Contact)
+
+# @admin.register(Contact)
+# class ContactData(admin.ModelAdmin):
+#     list_display=["id","full_name","phone","email","country"]
+#     search_fields=["email"]
